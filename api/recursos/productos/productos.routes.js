40190910b64ee0139046c630e8cc0d7cc091afda
nayruthCalla <nayruth.calla@gmail.com>
2019-08-05 19:32:44 -0500

    mostrando error como mensaje, a una peticion tipo POST

--- api/recursos/productos/productos.routes.js
+++ api/recursos/productos/productos.routes.js
@@ -6,19 +6,22 @@ const productos = require('../../../db').productos;
 const productsRoutes = express.Router();
 
 const blueprintProducto = Joi.object().keys({
-  titulo: Joi.string().min(3).max(100).required().error(() => 'error en la propiedad título'),
-  precio: Joi.number().precision(2).strict().required().error(() => 'error en la propiedad precio'),
-  moneda: Joi.string().max(3).required().error(() => 'error en la propiedad moneda'),
+  titulo: Joi.string().min(3).max(100).required().error(() => 'Error: '),
+  precio: Joi.number().precision(2).strict().required().error(() => 'Error: '),
+  moneda: Joi.string().max(3).required().error(() => 'Error: '),
 });
 function validateProducto(req, res, next) {
   const joiResult = Joi.validate(req.body, blueprintProducto,{abortEarly: false});
+  console.log(joiResult.error)
   let err = []
+  let errorMessage = '';
   if (joiResult.error) { // TODO: Mejorar el mensaje de error.    
     joiResult.error.details.forEach(element => {
-      err.push({message: element.message,type:element.type,context: element.context})      
+      err.push({message: element.message,type:element.type,context: element.context})
+      errorMessage += `${element.message} ${element.context.label}, tipo de error: ${element.type}, el límite es: ${element.context.limit}, el valor a cambiar: ${element.context.value}\n`     
     })
-    console.log(err)
-    res.status(400).send(err);
+    console.log(errorMessage)
+    res.status(400).send(errorMessage);
     return;
   }
   next();
